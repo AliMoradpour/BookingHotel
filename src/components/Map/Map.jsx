f9e0790840facfc0077495295d274e3a62ff9d05
AliMoradpour <mr.mrdp@yahoo.com>
2023-12-12 22:03:41 +0330

    add GeoLocation hook for use user location

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,6 +2,7 @@ import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useHotels } from "../context/HotelsProvider";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import useGeoLocation from "../../hooks/useGeoLocation";
 
 const Map = () => {
   const { isLoading, hotels } = useHotels();
@@ -10,9 +11,20 @@ const Map = () => {
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
+  const {
+    isLoading: isLoadingPosition,
+    position: geoLocationPosition,
+    getPosition,
+  } = useGeoLocation();
+
   useEffect(() => {
     if (lat && lng) setMapCenter([lat, lng]);
   }, [lat, lng]);
+
+  useEffect(() => {
+    if (geoLocationPosition?.lat && geoLocationPosition?.lng)
+      setMapCenter([geoLocationPosition.lat, geoLocationPosition.lng]);
+  }, [geoLocationPosition]);
 
   return (
     <div className="mapContainer">
@@ -25,6 +37,10 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
+        <button onClick={getPosition} className="getLocation">
+          {isLoadingPosition ? "Loading ..." : "Use Your Location"}
+        </button>
+
         <ChangeCenter position={mapCenter} />
         {hotels.map((item) => (
           <Marker key={item.id} position={[item.latitude, item.longitude]}>
