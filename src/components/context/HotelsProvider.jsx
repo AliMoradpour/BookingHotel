1da362190364d121ccd0ef639838092fdaf61fda
AliMoradpour <mr.mrdp@yahoo.com>
2023-12-13 23:43:46 +0330

    current hotel as context

--- src/components/context/HotelsProvider.jsx
+++ src/components/context/HotelsProvider.jsx
@@ -1,28 +1,49 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
-
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const HotelContext = createContext();
+const BASE_URL = "http://localhost:5000/hotels";
 
-const HotelsProvider = ({children}) => {
-    const [searchParams, setSearchParams] = useSearchParams();
-    const destination = searchParams.get("destination");
-    const room = JSON.parse(searchParams.get("options"))?.room;
-    const { isLoading, data:hotels } = useFetch(
-      "http://localhost:5000/hotels",
-      `q=${destination || ""}&accommodates_gte=${room || 1}`
-    );
+const HotelsProvider = ({ children }) => {
+  const [currentHotel, setCurrentHotel] = useState(null);
+  const [isLoadingCurrentHotel, setIsLoadingCurrentHotel] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const destination = searchParams.get("destination");
+  const room = JSON.parse(searchParams.get("options"))?.room;
+  const { isLoading, data: hotels } = useFetch(
+    BASE_URL,
+    `q=${destination || ""}&accommodates_gte=${room || 1}`
+  );
+
+  async function getHotel(id) {
+    setIsLoadingCurrentHotel(true);
+    try {
+      const { data } = await axios.get(`${BASE_URL}/${id}`);
+      setIsLoadingCurrentHotel(false);
+      setCurrentHotel(data);
+    } catch (error) {
+      toast.error(error.message);
+      setCurrentHotel(data);
+    }
+  }
 
   return (
-    <HotelContext.Provider value={{isLoading , hotels}}>
-        {children}
+    <HotelContext.Provider
+      value={{
+        isLoading,
+        hotels,
+        currentHotel,
+        getHotel,
+        isLoadingCurrentHotel,
+      }}>
+      {children}
     </HotelContext.Provider>
-  )
-}
+  );
+};
 
-export default HotelsProvider
+export default HotelsProvider;
 
-export const useHotels = ()=>(
-    useContext(HotelContext)
-)
+export const useHotels = () => useContext(HotelContext);
